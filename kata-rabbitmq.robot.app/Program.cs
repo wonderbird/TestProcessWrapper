9f40953dd35e387be1137962364aa74183e9195b
Stefan Boos <stefan.boos@gmx.de>
2020-11-12 05:51:28 +0100

    reverted docker related changes - moving them to branch dockerize

--- kata-rabbitmq.robot.app/Program.cs
+++ kata-rabbitmq.robot.app/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using Microsoft.Extensions.Logging;
 using RabbitMQ.Client;
 
@@ -21,44 +20,23 @@ namespace kata_rabbitmq.robot.app
             
             logger.LogDebug("Connecting to RabbitMQ ...");
 
-            IModel channel = null;
-            IConnection connection = null;
-            while (channel == null)
-            {
-                try
-                {
-                    var connectionFactory = new ConnectionFactory();
-                    connectionFactory.HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOSTNAME");
-                    var portString = Environment.GetEnvironmentVariable("RABBITMQ_PORT");
-                    if (portString != null) connectionFactory.Port = int.Parse(portString);
-                    connectionFactory.UserName = Environment.GetEnvironmentVariable("RABBITMQ_USERNAME");
-                    connectionFactory.Password = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD");
-                    connectionFactory.VirtualHost = "/";
-                    connectionFactory.ClientProvidedName = "app:robot";
+            var connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = Environment.GetEnvironmentVariable("RABBITMQ_HOSTNAME");
+            var portString = Environment.GetEnvironmentVariable("RABBITMQ_PORT");
+            if (portString != null) connectionFactory.Port = int.Parse(portString);
+            connectionFactory.UserName = Environment.GetEnvironmentVariable("RABBITMQ_USERNAME");
+            connectionFactory.Password = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD");
+            connectionFactory.VirtualHost = "/";
+            connectionFactory.ClientProvidedName = "app:robot";
 
-                    connection = connectionFactory.CreateConnection();
-                    channel = connection.CreateModel();
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
 
-                    channel.ExchangeDeclare("robot", ExchangeType.Direct, durable: false, autoDelete: true, arguments: null);
-                    channel.QueueDeclare("sensors", durable: false, exclusive: false, autoDelete: true, arguments: null);
+            channel.ExchangeDeclare("robot", ExchangeType.Direct, durable: false, autoDelete: true, arguments: null);
+            channel.QueueDeclare("sensors", durable: false, exclusive: false, autoDelete: true, arguments: null);
 
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    channel = null;
-                    connection = null;
-                    Thread.Sleep(500);
-                }
-            }
-
-            logger.Log(LogLevel.Information, "Enter 'stop' to shutdown the robot.");
-            string command;
-            do
-            {
-                command = Console.ReadLine();
-            }
-            while (command != "stop");
+            logger.Log(LogLevel.Information, "Press ENTER to shutdown the robot.");
+            Console.ReadLine();
             logger.Log(LogLevel.Information, "Shutting down ...");
             
             channel.Close();
