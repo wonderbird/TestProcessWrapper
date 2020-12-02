f367ac58e9ef3cf6596e64aa7065fc21570816a1
Stefan Boos <stefan.boos@gmx.de>
2020-12-02 05:08:31 +0100

    refactor: extract base class RabbitMqConnectedService

--- kata-rabbitmq.client.app/SensorDataConsumer.cs
+++ kata-rabbitmq.client.app/SensorDataConsumer.cs
@@ -1,72 +1,13 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using kata_rabbitmq.infrastructure;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace kata_rabbitmq.client.app
 {
-    public class SensorDataConsumer : BackgroundService
+    public class SensorDataConsumer : RabbitMqConnectedService
     {
-        private readonly IRabbitMqConnection _rabbit;
-        private readonly ILogger<SensorDataConsumer> _logger;
-
         public SensorDataConsumer(IRabbitMqConnection rabbit, ILogger<SensorDataConsumer> logger)
+            : base(rabbit, logger)
         {
-            _rabbit = rabbit;
-            _logger = logger;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            try
-            {
-                RegisterCancellationRequest(stoppingToken);
-                
-                while (true)
-                {
-                    await ExecuteSensorLoopBody(stoppingToken);
-                }
-            }
-            catch (OperationCanceledException)
-            {
-                // This exception is desired, when shutdown is requested. No action is necessary.
-            }
-            catch (Exception e)
-            {
-                _logger.LogCritical(e.ToString());
-            }
-            finally
-            {
-                ShutdownService();
-            }
-        }
-        
-        private void RegisterCancellationRequest(CancellationToken stoppingToken)
-        {
-            _logger.LogInformation("Waiting for cancellation request");
-            stoppingToken.Register(() => _logger.LogInformation("STOP request received"));
-            stoppingToken.ThrowIfCancellationRequested();
-        }
-
-        private async Task ExecuteSensorLoopBody(CancellationToken stoppingToken)
-        {
-            if (!_rabbit.IsConnected)
-            {
-                _rabbit.TryConnect();
-            }
-
-            await Task.Delay(TimeSpan.FromSeconds(2), stoppingToken);
-        }
-
-        private void ShutdownService()
-        {
-            _logger.LogInformation("Shutting down ...");
-
-            _rabbit.Disconnect();
-
-            _logger.LogDebug("Shutdown complete.");
         }
     }
 }
--- kata-rabbitmq.robot.app/SensorDataSender.cs
+++ kata-rabbitmq.robot.app/SensorDataSender.cs
@@ -1,74 +1,13 @@
-using System;
-using System.Threading;
-using System.Threading.Tasks;
 using kata_rabbitmq.infrastructure;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace kata_rabbitmq.robot.app
 {
     public class SensorDataSender : RabbitMqConnectedService
     {
-        private readonly IRabbitMqConnection _rabbit;
-        private readonly ILogger<SensorDataSender> _logger;
-
         public SensorDataSender(IRabbitMqConnection rabbit, ILogger<SensorDataSender> logger)
+            : base(rabbit, logger)
         {
-            _logger = logger;
-            _rabbit = rabbit;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            await base.ExecuteAsync(stoppingToken);
-            
-            try
-            {
-                RegisterCancellationRequest(stoppingToken);
-                
-                while (true)
-                {
-                    await ExecuteSensorLoopBody(stoppingToken);
-                }
-            }
-            catch (OperationCanceledException)
-            {
-                // This exception is desired, when shutdown is requested. No action is necessary.
-            }
-            catch (Exception e)
-            {
-                _logger.LogCritical(e.ToString());
-            }
-            finally
-            {
-                ShutdownService();
-            }
-        }
-
-        private void RegisterCancellationRequest(CancellationToken stoppingToken)
-        {
-            _logger.LogInformation("Waiting for cancellation request");
-            stoppingToken.Register(() => _logger.LogInformation("STOP request received"));
-            stoppingToken.ThrowIfCancellationRequested();
-        }
-
-        private async Task ExecuteSensorLoopBody(CancellationToken stoppingToken)
-        {
-            if (!_rabbit.IsConnected)
-            {
-                _rabbit.TryConnect();
-            }
-            
-            await Task.Delay(TimeSpan.FromSeconds(2), stoppingToken);
-        }
-
-        private void ShutdownService()
-        {
-            _logger.LogInformation("Shutting down ...");
-
-            _rabbit.Disconnect();
-
-            _logger.LogDebug("Shutdown complete.");
         }
     }
 }
