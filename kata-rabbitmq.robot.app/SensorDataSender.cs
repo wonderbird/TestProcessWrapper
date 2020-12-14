14382b750f5a8ac5812035c1fda7dea5770ea9f9
Stefan Boos <stefan.boos@gmx.de>
2020-12-14 06:08:58 +0100

    feat: add message publishing to robot (wip)

--- kata-rabbitmq.robot.app/SensorDataSender.cs
+++ kata-rabbitmq.robot.app/SensorDataSender.cs
@@ -1,13 +1,36 @@
 ﻿using katarabbitmq.infrastructure;
 using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
 
 namespace katarabbitmq.robot.app
 {
     public class SensorDataSender : RabbitMqConnectedService
     {
+        private ILogger<SensorDataSender> _logger;
+
         public SensorDataSender(IRabbitMqConnection rabbit, ILogger<SensorDataSender> logger)
             : base(rabbit, logger)
         {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteSensorLoopBody(CancellationToken stoppingToken)
+        {
+            await base.ExecuteSensorLoopBody(stoppingToken);
+
+            if (Rabbit.IsConnected)
+            {
+                TODO: Routing Key muss noch an die Exchange gebunden werden
+                    
+                var message = "Es ist heiß hier.";
+                var body = Encoding.UTF8.GetBytes(message);
+                Rabbit.Channel.BasicPublish("robot", "sensors", null, body);
+                
+                _logger.LogInformation("Sent a message");
+            }
         }
     }
 }
