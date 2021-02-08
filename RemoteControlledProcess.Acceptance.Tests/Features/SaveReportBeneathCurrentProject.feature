a16355ec801019057f6d79bc4af5a719da155fc4
Stefan Boos <stefan.boos@gmx.de>
2021-02-08 05:57:49 +0100

    test(acceptance): implement feature test

--- RemoteControlledProcess.Acceptance.Tests/Features/SaveReportBeneathCurrentProject.feature
+++ RemoteControlledProcess.Acceptance.Tests/Features/SaveReportBeneathCurrentProject.feature
@@ -3,8 +3,7 @@ Feature: Save Report Beneath Current Project
   In order to have a clean project structure
   As a developer using the NuGet package
   I want generated coverage reports to be stored in the TestResults folder of the current project
-
-  @Ignore
+  
   Scenario: Generate coverage report in TestResults folder of current project
     Given the number of coverage reports in the TestResults folder is known
     And 1 application is running with coverlet 'enabled'
--- RemoteControlledProcess.Acceptance.Tests/Steps/SaveReportBeneathCurrentProjectStepDefinition.cs
+++ RemoteControlledProcess.Acceptance.Tests/Steps/SaveReportBeneathCurrentProjectStepDefinition.cs
@@ -1,25 +1,42 @@
+using System.IO;
+using System.Linq;
 using TechTalk.SpecFlow;
+using Xunit;
 
 namespace katarabbitmq.bdd.tests.Steps
 {
+    [Binding]
     public class SpecifyReportDirectoryStepDefinition
     {
-        private readonly ScenarioContext _scenarioContext;
+        private int _initialNumberOfCoverageReports;
 
-        public SpecifyReportDirectoryStepDefinition(ScenarioContext scenarioContext)
-        {
-            _scenarioContext = scenarioContext;
-        }
         [Given(@"the number of coverage reports in the TestResults folder is known")]
         public void GivenTheNumberOfCoverageReportsInTheTestResultsFolderIsKnown()
         {
-            _scenarioContext.Pending();
+            _initialNumberOfCoverageReports = CountTestResultFiles();
+        }
+
+        private static int CountTestResultFiles()
+        {
+            try
+            {
+                var directory = Path.Join("..", "..", "..", "TestResults");
+                var searchPattern = "RemoteControlledProcess.Application.*.xml";
+
+                return Directory.EnumerateFiles(directory, searchPattern, SearchOption.TopDirectoryOnly)
+                    .Count();
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return 0;
+            }
         }
 
         [Then(@"an additional coverage report exists in the TestResults folder")]
         public void ThenAnAdditionalCoverageReportExistsInTheTestResultsFolder()
         {
-            _scenarioContext.Pending();
+            var actualNumberOfCoverageReports = CountTestResultFiles();
+            Assert.Equal(_initialNumberOfCoverageReports + 1, actualNumberOfCoverageReports);
         }
     }
 }
