306fc6b60766aecf52fd48b8ac07520e591993d3
Stefan Boos <stefan.boos@gmx.de>
2021-02-17 06:58:57 +0100

    revert: chore: add logging to unit test failing under windows

--- RemoteControlledProcess.Unit.Tests/ExceptionReporterExtensionTests.cs
+++ RemoteControlledProcess.Unit.Tests/ExceptionReporterExtensionTests.cs
@@ -2,16 +2,12 @@ using System.IO;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace RemoteControlledProcess.Unit.Tests
 {
     public class ExceptionReporterExtensionTests
     {
         private const string ExceptionMessage = "Exception generated for test purpose";
-        private ITestOutputHelper _testOutputHelper;
-
-        public ExceptionReporterExtensionTests(ITestOutputHelper testOutputHelper) => _testOutputHelper = testOutputHelper;
 
         [Theory]
         [InlineData(0,
@@ -34,12 +30,6 @@ namespace RemoteControlledProcess.Unit.Tests
 
             // Assert
             var actualMessage = (string)writerMock.Invocations[invocationIndex].Arguments[0];
-
-            _testOutputHelper.WriteLine("===== Expected Message Regex:");
-            _testOutputHelper.WriteLine($"\"{expectedMessageRegex}\"");
-            _testOutputHelper.WriteLine("===== Actual Message:");
-            _testOutputHelper.WriteLine($"\"{actualMessage}\"");
-
             Assert.Matches(expectedMessageRegex, actualMessage);
         }
 
@@ -64,17 +54,6 @@ namespace RemoteControlledProcess.Unit.Tests
                 @"Unhandled exception in .*RemoteControlledProcess\.Unit\.Tests[\/\\]ExceptionReporterExtensionTests\.cs:[0-9]+";
             var actualLogLevel = (LogLevel)loggerMock.Invocations[0].Arguments[0];
             var actualMessage = loggerMock.Invocations[0].Arguments[2].ToString();
-
-            _testOutputHelper.WriteLine("===== Expected Message Regex:");
-            _testOutputHelper.WriteLine($"\"{expectedMessageRegex}\"");
-            _testOutputHelper.WriteLine("===== Actual Message:");
-            _testOutputHelper.WriteLine($"\"{actualMessage}\"");
-
-            _testOutputHelper.WriteLine("===== Expected LogLevel:");
-            _testOutputHelper.WriteLine($"\"{LogLevel.Critical}\"");
-            _testOutputHelper.WriteLine("===== Actual LogLevel:");
-            _testOutputHelper.WriteLine($"\"{actualLogLevel}\"");
-
             Assert.Equal(LogLevel.Critical, actualLogLevel);
             Assert.Matches(expectedMessageRegex, actualMessage);
         }
