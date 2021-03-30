f9acc997eaf28b44f46df2e5bf13404d4253c564
Stefan Boos <stefan.boos@gmx.de>
2021-03-30 07:39:13 +0200

    chore: move TestProjectInfo to its own file

--- RemoteControlledProcess/ProcessWrapper.cs
+++ RemoteControlledProcess/ProcessWrapper.cs
@@ -7,31 +7,6 @@ using Xunit.Abstractions;
 
 namespace RemoteControlledProcess
 {
-    // TODO extract class to separate file
-    class TestProjectInfo
-    {
-        public TestProjectInfo(string appProjectName)
-        {
-            var projectRelativeDir = Path.Combine("..", "..", "..", "..");
-            ProjectDir = Path.GetFullPath(projectRelativeDir);
-            AppProjectName = appProjectName;
-        }
-
-        public string ProjectDir { get; }
-        public string AppProjectName { get; }
-        public string AppDllName => AppProjectName + ".dll";
-        public string CoverageReportPath
-        {
-            get
-            {
-                var coverageReportFileName = $"{AppProjectName}.{Guid.NewGuid().ToString()}.xml";
-                var coverageReportRelativeDir = Path.Join("..", "..", "..", "TestResults");
-                var coverageReportDir = Path.GetFullPath(coverageReportRelativeDir);
-                return Path.Combine(coverageReportDir, coverageReportFileName);
-            }
-        }
-    }
-
     // TODO Rename file to match class name
     public sealed class TestProcessWrapper : IDisposable
     {
