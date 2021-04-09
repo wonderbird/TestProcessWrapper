65551c89ad6b03c52fd09da3e62ed98219818da2
Stefan Boos <stefan.boos@gmx.de>
2021-04-09 06:39:57 +0200

    refactor: extract CreateKillStrategy method in ProcessKillerFactory

--- RemoteControlledProcess/ProcessKillerFactory.cs
+++ RemoteControlledProcess/ProcessKillerFactory.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.Runtime.InteropServices;
 using Xunit.Abstractions;
 
@@ -11,14 +12,12 @@ namespace RemoteControlledProcess
 
         private ITestOutputHelper TestOutputHelper { get; }
 
-        private Func<int?, Process> CreateUnixStrategy()
+        private Func<int?, Process> CreateKillStrategy(string killCommand, string killArgumentsWithPidPlaceholder, string signalName)
         {
             return pid =>
             {
-                var killCommand = "kill";
                 // ReSharper disable once PossibleInvalidOperationException
-                var killArguments = $"-s TERM {pid.Value}";
-                var signalName = "TERM";
+                var killArguments = string.Format(CultureInfo.InvariantCulture, killArgumentsWithPidPlaceholder, pid.Value);
                 TestOutputHelper?.WriteLine($"Sending {signalName} signal to process ...");
                 TestOutputHelper?.WriteLine($"Invoking system call: {killCommand} {killArguments}");
                 var killProcess = Process.Start(killCommand, killArguments);
@@ -26,30 +25,18 @@ namespace RemoteControlledProcess
             };
         }
 
+        private Func<int?, Process> CreateUnixStrategy() => CreateKillStrategy("kill", "-s TERM {0}", "TERM");
 
-        private Func<int?, Process> CreateWindowsStrategy()
-        {
-            return pid =>
-            {
-                var killCommand = "taskkill";
-                // ReSharper disable once PossibleInvalidOperationException
-                var killArguments = $"/f /pid {pid.Value}";
 
-                // Under Windows, SIGINT doesn't work. Thus we use the KILL signal.
-                //
-                // To try this out you can place a breakpoint here and check on the
-                // command line yourself.
-                //
-                // This can be tolerated for our case here, because the application
-                // is intended to run in a linux docker container and because the
-                // build pipeline uses linux containers for testing.
-                var signalName = "KILL";
-                TestOutputHelper?.WriteLine($"Sending {signalName} signal to process ...");
-                TestOutputHelper?.WriteLine($"Invoking system call: {killCommand} {killArguments}");
-                var killProcess = Process.Start(killCommand, killArguments);
-                return killProcess;
-            };
-        }
+        // Under Windows, SIGINT doesn't work. Thus we use the KILL signal.
+        //
+        // To try this out you can place a breakpoint here and check on the
+        // command line yourself.
+        //
+        // This can be tolerated for our case here, because the application
+        // is intended to run in a linux docker container and because the
+        // build pipeline uses linux containers for testing.
+        private Func<int?, Process> CreateWindowsStrategy() => CreateKillStrategy("taskkill", "/f /pid {0}", "KILL");
 
         public Func<int?, Process> CreateProcessKillingMethod() =>
             RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? CreateWindowsStrategy() : CreateUnixStrategy();
