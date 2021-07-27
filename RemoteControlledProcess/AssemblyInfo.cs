e1a04e2cdad7b5e0842cac6d8f875a953ff0fa92
Stefan Boos <kontakt@boos.systems>
2021-07-27 06:18:06 +0200

    test: fix access to internals for Moq

--- RemoteControlledProcess/AssemblyInfo.cs
+++ RemoteControlledProcess/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("RemoteControlledProcess.Unit.Tests")]
+[assembly: InternalsVisibleTo("RemoteControlledProcess.Unit.Tests"),
+           InternalsVisibleTo("DynamicProxyGenAssembly2")]
