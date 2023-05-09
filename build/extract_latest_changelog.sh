fec9a83e53a7a2a816db9281183b96a319aa964f
Stefan Boos <kontakt@boos.systems>
2023-05-09 08:16:57 +0200

    fix(ci): suppress error when constructing awk script

--- build/extract_latest_changelog.sh
+++ build/extract_latest_changelog.sh
@@ -2,9 +2,19 @@
 #
 # Extract the introduction and the entry for the latest version from CHANGELOG.md
 #
+# WARNING: Duplicate Code
+#
+# This script is maintained in two different GitHub repositories. The "copy" is in
+# https://github.com/wonderbird/malimo/blob/main/build/extract_latest_changelog.sh
+#
+# Refactor as soon as a third copy is created.
+#
 set -euxf
 
-read -rd '' extract_intro_and_latest_release << 'EOF'
+# The if construct around the read command suppresses the error returned by read.
+# read returns 1, because it does not find the delimiter '' in the input.
+# See also: https://stackoverflow.com/questions/40547032/bash-read-returns-with-exit-code-1-even-though-it-runs-as-expected#67066283
+if read -rd '' extract_intro_and_latest_release; then :; fi << 'EOF'
 BEGIN {
   number_of_parsed_version_headings = 0
 }
