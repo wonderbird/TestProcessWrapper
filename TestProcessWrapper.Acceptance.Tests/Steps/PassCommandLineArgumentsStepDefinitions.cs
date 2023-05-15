2a4731a713075e301b13881e4afb4a5cc7bd2b5b
Stefan Boos <kontakt@boos.systems>
2023-05-15 05:25:39 +0200

    refactor(test): reuse regex to match arguments by extracting to constant

--- TestProcessWrapper.Acceptance.Tests/Steps/PassCommandLineArgumentsStepDefinitions.cs
+++ TestProcessWrapper.Acceptance.Tests/Steps/PassCommandLineArgumentsStepDefinitions.cs
@@ -7,9 +7,10 @@ namespace TestProcessWrapper.Acceptance.Tests.Steps;
 [Binding]
 public class StepDefinitions
 {
+    private const string AllowedCharactersForArgument = "[-a-zA-Z0-9]*";
     private string _outputWhenReady;
 
-    [Given(@"the command line argument '([-a-zA-Z0-9]*)=([-a-zA-Z0-9]*)' has been configured")]
+    [Given($"the command line argument '({AllowedCharactersForArgument})=({AllowedCharactersForArgument})' has been configured")]
     public void GivenTheCommandLineArgumentHasBeenConfigured(string argument, string value)
     {
         var client = SingleProcessControlStepDefinitions.Client;
@@ -21,7 +22,7 @@ public class StepDefinitions
         });
     }
 
-    [Given(@"the command line argument '([-a-zA-Z0-9]*)' has been configured")]
+    [Given($"the command line argument '({AllowedCharactersForArgument})' has been configured")]
     public void GivenTheCommandLineArgumentHasBeenConfigured(string argument)
     {
         var client = SingleProcessControlStepDefinitions.Client;
@@ -33,13 +34,13 @@ public class StepDefinitions
         });
     }
 
-    [Then(@"the application has received the command line argument '([-a-zA-Z0-9]*)' with value '([-a-zA-Z0-9]*)'")]
+    [Then($"the application has received the command line argument '({AllowedCharactersForArgument})' with value '({AllowedCharactersForArgument})'")]
     public void ThenTheApplicationHasReceivedTheCommandLineArgumentWithValue(string argument, string value)
     {
         Assert.Contains($"Received the command line argument '{argument}={value}'", _outputWhenReady);
     }
 
-    [Then(@"the application has received the command line argument '([-a-zA-Z0-9]*)'")]
+    [Then($"the application has received the command line argument '({AllowedCharactersForArgument})'")]
     public void ThenTheApplicationHasReceivedTheCommandLineArgument(string argument)
     {
         Assert.Contains($"Received the command line argument '{argument}'", _outputWhenReady);
