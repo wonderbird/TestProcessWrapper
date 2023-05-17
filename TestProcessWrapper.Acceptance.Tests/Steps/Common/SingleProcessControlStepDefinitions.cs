b33e399cab8b15d027f26e34448e3e3f518e7906
Stefan Boos <kontakt@boos.systems>
2023-05-17 05:44:58 +0200

    test: ensure single process is actually launched

--- TestProcessWrapper.Acceptance.Tests/Steps/Common/SingleProcessControlStepDefinitions.cs
+++ TestProcessWrapper.Acceptance.Tests/Steps/Common/SingleProcessControlStepDefinitions.cs
@@ -1,5 +1,6 @@
 using System;
 using TechTalk.SpecFlow;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace TestProcessWrapper.Acceptance.Tests.Steps.Common
@@ -61,6 +62,7 @@ namespace TestProcessWrapper.Acceptance.Tests.Steps.Common
         public static void WhenTheApplicationIsReady()
         {
             Client.Start();
+            Assert.True(Client.IsRunning, "test process must be in 'running' state right after starting it");
         }
 
         [AfterScenario]
